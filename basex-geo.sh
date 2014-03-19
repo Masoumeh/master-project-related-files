961408e2bd58a94109cced8baf41a9fd75fbf80a
zi54wog <masoumeh.seydi@gmail.com>
2014-03-19 09:58:20 +0100

    needed library added, some shell files regarding the time testing added

---
+++ basex-geo.sh
@@ -0,0 +1,3 @@
+java -Xrunhprof:cpu=samples,depth=15 -cp /home/rostam/Desktop/basex-indexing/basex/target/classes:/home/rostam/Desktop/basex-api/target/classes:/home/rostam/Desktop/important/BaseX/jts-1.13/lib/jts-1.13.jar org.basex.BaseX "$@"
+
+
